73f145a1315a42936577e784cdadd20047dd6ed1
AdamAZCS <adam@azcs.at>
2023-03-11 08:45:08 +0100

    preliminary steps towards forecast diagrams creation

--- src/Screens/WeatherForecastScreen.cpp
+++ src/Screens/WeatherForecastScreen.cpp
@@ -5,6 +5,77 @@
 
 using namespace Watchy;
 
+  typedef struct int8_stats {
+    int8_t min;
+    int8_t max;
+    int8_t range;
+  } int8_stats_t;
+
+  typedef struct int16_stats {
+    int16_t min;
+    int16_t max;
+    int16_t range;
+  } int16_stats_t;
+
+  typedef struct float_stats {
+    float min;
+    float max;
+    float range;
+  } float_stats_t;
+
+  typedef struct stats {
+    int8_stats_t temperature;
+    int16_stats pressure;
+    float_stats windSpeed;
+  } stats_t;
+
+void initStats(int8_stats &stats, int8_t value){
+    stats.min = value;
+    stats.max = value;
+    stats.range = 0;
+}
+void updateStats(int8_stats &stats, int8_t value){
+    if( value < stats.min )
+      stats.min = value;
+    if( value > stats.max )
+      stats.max = value;
+    stats.range = stats.max - stats.min;
+}
+void initStats(int16_stats &stats, int16_t value){
+    stats.min = value;
+    stats.max = value;
+    stats.range = 0;
+}
+void updateStats(int16_stats &stats, int16_t value){
+    if( value < stats.min )
+      stats.min = value;
+    if( value > stats.max )
+      stats.max = value;
+    stats.range = stats.max - stats.min;
+}
+void initStats(float_stats &stats, float value){
+    stats.min = value;
+    stats.max = value;
+    stats.range = 0;
+}
+void updateStats(float_stats &stats, float value){
+    if( value < stats.min )
+      stats.min = value;
+    if( value > stats.max )
+      stats.max = value;
+    stats.range = stats.max - stats.min;
+}
+void initStats(stats_t &stats, Watchy_GetWeather::weatherData weather){
+    initStats(stats.temperature ,weather.temperature);
+    initStats(stats.pressure    ,weather.pressure);
+    initStats(stats.windSpeed   ,weather.wind.speed);
+}
+void updateStats(stats_t &stats, Watchy_GetWeather::weatherData weather){
+    updateStats(stats.temperature ,weather.temperature);
+    updateStats(stats.pressure    ,weather.pressure);
+    updateStats(stats.windSpeed   ,weather.wind.speed);
+}
+
 
 void WeatherForecastScreen::show() {
   Watchy::RTC.setRefresh(RTC_REFRESH_NONE);
@@ -25,26 +96,39 @@ void WeatherForecastScreen::show() {
   );
   */
 
-  int8_t minTemp = 0;
-  int8_t maxTemp = 0;
-  int8_t tempRange = 0;
+
+  stats_t stats;
+
+  initStats(stats,Watchy_GetWeather::currentWeather);
+
+//  int8_stats_t   tempStats = {Watchy_GetWeather::currentWeather.temperature,Watchy_GetWeather::currentWeather.temperature,0};
+//  int16_stats_t presStats = {Watchy_GetWeather::currentWeather.pressure,Watchy_GetWeather::currentWeather.pressure,0};
+//  float_stats_t wndsStats = {Watchy_GetWeather::currentWeather.wind.speed,Watchy_GetWeather::currentWeather.wind.speed,0};
 
   log_i("nr_forecasts: %d", Watchy_GetWeather::nr_forecasts);
 
   for( int i = 0; i < Watchy_GetWeather::nr_forecasts; i++ ) {
     Watchy_GetWeather::weatherData forecast = Watchy_GetWeather::forecastWeather[i];
-    int8_t temp = forecast.temperature;
-    if( temp < minTemp )
-      minTemp = temp;
-    if( temp > maxTemp )
-      maxTemp = temp;
-    log_i("forecast %d: temp: %d min: %d max: %d", i, temp, minTemp, maxTemp);
+    updateStats(stats,forecast);
+//    updateStats(tempStats,forecast.temperature);
+//    updateStats(presStats,forecast.pressure);
+//    updateStats(wndsStats,forecast.pressure);
+//    if( temp < tempStats.min )
+//      tempStats.min = temp;
+//    if( temp > tempStats.max )
+//      tempStats.max = temp;
+//    log_d("forecast %d: temp: %d min: %d max: %d", i, temp, minTemp, maxTemp);
   }
-  tempRange = maxTemp - minTemp;
-  log_i("tempRange: %d", tempRange); 
-  display.printf("\n\nnr_forecasts:%d\ntemp: %d - %d : %d"
+//  tempStats.range = tempStats.max - tempStats.min;
+//  presStats.range = presStats.max - presStats.min;
+//  wndsStats.range = wndsStats.max - wndsStats.min;
+
+  int8_stats_t   tempStats = stats.temperature;
+
+  log_d("tempRange: %d", tempStats.range); 
+  Watchy::display.printf("\n\nnr_forecasts:%d\ntemp: %d - %d : %d"
     , Watchy_GetWeather::nr_forecasts
-    , minTemp, maxTemp, tempRange
+    , tempStats.min, tempStats.max, tempStats.range
   );
 
   float fgColor = (bgColor == GxEPD_WHITE ? GxEPD_BLACK : GxEPD_WHITE);
@@ -54,12 +138,13 @@ void WeatherForecastScreen::show() {
   const int16_t x0 = 200 - width;
   const int16_t y0 = 120;
 
-  Watchy::display.drawLine(x0, y0,   199, y0 + height,   fgColor);
+//  Watchy::display.drawLine(x0, y0,   199, y0 + height,   fgColor);
 //  Watchy::display.drawLine(x0, y0+1, 199, y0 + height+1, fgColor);
   
-  const float scale = ((float) height) / ((float) tempRange);
+  const float scale = ((float) height) / ((float) tempStats.range);
+
   int16_t x = x0;
-  int16_t y = y0;
+  int16_t y = y0 + height - int( ( ((float)(Watchy_GetWeather::currentWeather.temperature - tempStats.min)) *  scale) + 0.5);;
 
   for( int i = 0; i < Watchy_GetWeather::nr_forecasts; i++ ) {
     Watchy_GetWeather::weatherData forecast = Watchy_GetWeather::forecastWeather[i];
@@ -69,11 +154,13 @@ void WeatherForecastScreen::show() {
     const int16_t lastY = y;
 
     x += 3;
-    y = y0 + height - int( ( ((float)(temp - minTemp)) *  scale) + 0.5);
+    y = y0 + height - int( ( ((float)(temp - tempStats.min)) *  scale) + 0.5);
   
     Watchy::display.drawLine(lastX, lastY,   x, y,    fgColor);
     Watchy::display.drawLine(lastX, lastY+1, x, y+1,  fgColor);
     Watchy::display.drawLine(lastX, lastY-1, x, y-1,  fgColor);
+    Watchy::display.drawLine(lastX, lastY+2, x, y+2,  fgColor);
+    Watchy::display.drawLine(lastX, lastY-2, x, y-2,  fgColor);
     
   /*
       log_d("drawLine(%d, %d, %d, %d, fgColor)", x0 + i, y0 + height - h, x0 + i, y0 + height);

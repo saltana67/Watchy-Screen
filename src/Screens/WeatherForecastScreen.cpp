01c8256c3b4f6305242274194c9809b83f2afc9c
AdamAZCS <adam@azcs.at>
2023-03-13 05:22:21 +0100

    added drawWeatherTimeline function, made use of it for multiple lines, cleanups

--- src/Screens/WeatherForecastScreen.cpp
+++ src/Screens/WeatherForecastScreen.cpp
@@ -27,6 +27,8 @@ using namespace Watchy;
     int8_stats_t temperature;
     int16_stats pressure;
     float_stats windSpeed;
+    int8_stats_t clouds;
+    int8_stats_t humidity;
   } stats_t;
 
 void initStats(int8_stats &stats, int8_t value){
@@ -69,13 +71,72 @@ void initStats(stats_t &stats, Watchy_GetWeather::weatherData weather){
     initStats(stats.temperature ,weather.temperature);
     initStats(stats.pressure    ,weather.pressure);
     initStats(stats.windSpeed   ,weather.wind.speed);
+    initStats(stats.clouds      ,weather.clouds);
+    initStats(stats.humidity    ,weather.humidity);
 }
 void updateStats(stats_t &stats, Watchy_GetWeather::weatherData weather){
     updateStats(stats.temperature ,weather.temperature);
     updateStats(stats.pressure    ,weather.pressure);
     updateStats(stats.windSpeed   ,weather.wind.speed);
+    updateStats(stats.clouds      ,weather.clouds);
+    updateStats(stats.humidity    ,weather.humidity);
 }
 
+float getTemperature(Watchy_GetWeather::weatherData weather) {
+  return (float) weather.temperature;
+}
+float getPressure(Watchy_GetWeather::weatherData weather) {
+  return (float) weather.pressure;
+}
+float getClouds(Watchy_GetWeather::weatherData weather) {
+  return (float) weather.clouds;
+}
+float getWindSpeed(Watchy_GetWeather::weatherData weather) {
+  return (float) weather.wind.speed;
+}
+float getHumidity(Watchy_GetWeather::weatherData weather) {
+  return (float) weather.humidity;
+}
+
+void drawWeatherTimeline(const int16_t x0, const int16_t y0, 
+                    const int16_t width, const int16_t height,
+                    const float min, const float max,
+                    float (*getValue)(Watchy_GetWeather::weatherData),
+                    const uint16_t fgColor){
+  
+  const float scale = ((float) height) / (max - min);
+  log_d("scale: %f ", scale);
+
+  float value = getValue(Watchy_GetWeather::currentWeather);
+  log_d("first value: %d ", value);
+  int16_t x = x0;
+  int16_t y = y0 + height - int( ( ((float)(value - min)) *  scale) + 0.5);
+
+  for( int i = 0; i < Watchy_GetWeather::nr_forecasts; i++ ) {
+    Watchy_GetWeather::weatherData forecast = Watchy_GetWeather::forecastWeather[i];
+    value = getValue(forecast);
+
+    const int16_t lastX = x;
+    const int16_t lastY = y;
+
+    x += 3;
+    y = y0 + height - int( ( ((float)(value - min)) *  scale) + 0.5);
+
+    log_d("value: %d, line: %d:%d  -> %d:%d", value, lastX, lastY, x, y);
+  
+    Watchy::display.drawLine(lastX, lastY,   x, y,    fgColor);
+    Watchy::display.drawLine(lastX, lastY+1, x, y+1,  fgColor);
+    Watchy::display.drawLine(lastX, lastY-1, x, y-1,  fgColor);
+    Watchy::display.drawLine(lastX, lastY+2, x, y+2,  fgColor);
+    Watchy::display.drawLine(lastX, lastY-2, x, y-2,  fgColor);
+    
+  /*
+      log_d("drawLine(%d, %d, %d, %d, fgColor)", x0 + i, y0 + height - h, x0 + i, y0 + height);
+      Watchy::display.drawLine(x0 + i, y0 + height - h, x0 + i, y0 + height,
+                               fgColor);
+  */
+  }
+}
 
 void WeatherForecastScreen::show() {
   Watchy::RTC.setRefresh(RTC_REFRESH_NONE);
@@ -95,77 +156,42 @@ void WeatherForecastScreen::show() {
     weatherConditionCodeToString(wd.weatherConditionCode)
   );
   */
-
-
+ 
   stats_t stats;
 
   initStats(stats,Watchy_GetWeather::currentWeather);
-
-//  int8_stats_t   tempStats = {Watchy_GetWeather::currentWeather.temperature,Watchy_GetWeather::currentWeather.temperature,0};
-//  int16_stats_t presStats = {Watchy_GetWeather::currentWeather.pressure,Watchy_GetWeather::currentWeather.pressure,0};
-//  float_stats_t wndsStats = {Watchy_GetWeather::currentWeather.wind.speed,Watchy_GetWeather::currentWeather.wind.speed,0};
 
   log_i("nr_forecasts: %d", Watchy_GetWeather::nr_forecasts);
 
   for( int i = 0; i < Watchy_GetWeather::nr_forecasts; i++ ) {
     Watchy_GetWeather::weatherData forecast = Watchy_GetWeather::forecastWeather[i];
     updateStats(stats,forecast);
-//    updateStats(tempStats,forecast.temperature);
-//    updateStats(presStats,forecast.pressure);
-//    updateStats(wndsStats,forecast.pressure);
-//    if( temp < tempStats.min )
-//      tempStats.min = temp;
-//    if( temp > tempStats.max )
-//      tempStats.max = temp;
-//    log_d("forecast %d: temp: %d min: %d max: %d", i, temp, minTemp, maxTemp);
   }
-//  tempStats.range = tempStats.max - tempStats.min;
-//  presStats.range = presStats.max - presStats.min;
-//  wndsStats.range = wndsStats.max - wndsStats.min;
 
   int8_stats_t   tempStats = stats.temperature;
 
   log_d("tempRange: %d", tempStats.range); 
-  Watchy::display.printf("\n\nnr_forecasts:%d\ntemp: %d - %d : %d"
-    , Watchy_GetWeather::nr_forecasts
-    , tempStats.min, tempStats.max, tempStats.range
-  );
 
-  float fgColor = (bgColor == GxEPD_WHITE ? GxEPD_BLACK : GxEPD_WHITE);
+  uint16_t fgColor = (bgColor == GxEPD_WHITE ? GxEPD_BLACK : GxEPD_WHITE);
 
   const int16_t height = 50;
   const int16_t width  = 40 * 3; //3 pixel per forecast
   const int16_t x0 = 200 - width;
-  const int16_t y0 = 120;
 
-//  Watchy::display.drawLine(x0, y0,   199, y0 + height,   fgColor);
-//  Watchy::display.drawLine(x0, y0+1, 199, y0 + height+1, fgColor);
-  
-  const float scale = ((float) height) / ((float) tempStats.range);
+  drawWeatherTimeline(x0, 0, width, height, 
+                      (float) stats.temperature.min, (float) stats.temperature.max,
+                      getTemperature, fgColor);
 
-  int16_t x = x0;
-  int16_t y = y0 + height - int( ( ((float)(Watchy_GetWeather::currentWeather.temperature - tempStats.min)) *  scale) + 0.5);;
+  drawWeatherTimeline(x0, 50, width, height, 
+                      (float) stats.pressure.min, (float) stats.pressure.max,
+                      getPressure, fgColor);
 
-  for( int i = 0; i < Watchy_GetWeather::nr_forecasts; i++ ) {
-    Watchy_GetWeather::weatherData forecast = Watchy_GetWeather::forecastWeather[i];
-    int8_t temp = forecast.temperature;
+  drawWeatherTimeline(x0, 100, width, height, 
+                      (float) stats.humidity.min, (float) stats.humidity.max,
+                      getHumidity, fgColor);
 
-    const int16_t lastX = x;
-    const int16_t lastY = y;
+  drawWeatherTimeline(x0, 150, width, height, 
+                      (float) stats.windSpeed.min, (float) stats.windSpeed.max,
+                      getWindSpeed, fgColor);
 
-    x += 3;
-    y = y0 + height - int( ( ((float)(temp - tempStats.min)) *  scale) + 0.5);
-  
-    Watchy::display.drawLine(lastX, lastY,   x, y,    fgColor);
-    Watchy::display.drawLine(lastX, lastY+1, x, y+1,  fgColor);
-    Watchy::display.drawLine(lastX, lastY-1, x, y-1,  fgColor);
-    Watchy::display.drawLine(lastX, lastY+2, x, y+2,  fgColor);
-    Watchy::display.drawLine(lastX, lastY-2, x, y-2,  fgColor);
-    
-  /*
-      log_d("drawLine(%d, %d, %d, %d, fgColor)", x0 + i, y0 + height - h, x0 + i, y0 + height);
-      Watchy::display.drawLine(x0 + i, y0 + height - h, x0 + i, y0 + height,
-                               fgColor);
-  */
-  }
 }

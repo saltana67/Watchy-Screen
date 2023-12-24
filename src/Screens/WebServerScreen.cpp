9c05d429466a69e170b95fdcaf7698623de6af21
Cosmo Saltana <saltana67@gmail.com>
2023-12-24 04:19:52 +0100

    tiny fixes

--- src/Screens/WebServerScreen.cpp
+++ src/Screens/WebServerScreen.cpp
@@ -12,23 +12,32 @@ void WebServerScreen::show() {
   Watchy::display.fillScreen(bgColor);
   Watchy::display.setFont(OptimaLTStd12pt7b);
   
+  Watchy::display.print("\n");
   if( ! Watchy_WebServer::running ) 
-    Watchy::display.print("\nNOT ");
+    Watchy::display.print("NOT ");
   Watchy::display.print("running");
-  Watchy::display.print("\npress up/down to start");
+  if( Watchy_WebServer::running ) {
+    Watchy::display.print("\n@");
+    Watchy::display.print(WiFi.localIP().toString().c_str());
+    Watchy::display.print("\npress down to stop");
+  }else{
+    Watchy::display.print("\npress up to start");
+  }
   Watchy::display.print("\npress menu to update");
   
 }
 
 void WebServerScreen::up() {
   Watchy_WebServer::webServer.begin();
+  Watchy::showWatchFace(true);
 }
 void WebServerScreen::down() {
   Watchy_WebServer::stopServer();
+  Watchy::showWatchFace(true);
 }
 
 void WebServerScreen::menu() {
-  WebServerScreen::show();
+  Watchy::showWatchFace(true);
 }
 
 void WebServerScreen::back() {

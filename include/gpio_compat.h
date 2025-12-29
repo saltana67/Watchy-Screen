8cafde73b4ba097aec3c6fc65ea9ab3ce2c7a257
Saltana67 <saltana67@gmail.com>
2025-12-30 00:38:33 +0100

    include/gpio_compat.h: added new compatibility include

---
+++ include/gpio_compat.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <driver/gpio.h>
+
+#ifndef GPIO_SEL_4
+#define GPIO_SEL_4   (1ULL << GPIO_NUM_4)
+#endif
+#ifndef GPIO_SEL_14
+#define GPIO_SEL_14  (1ULL << GPIO_NUM_14)
+#endif
+#ifndef GPIO_SEL_25
+#define GPIO_SEL_25  (1ULL << GPIO_NUM_25)
+#endif
+#ifndef GPIO_SEL_26
+#define GPIO_SEL_26  (1ULL << GPIO_NUM_26)
+#endif
+#ifndef GPIO_SEL_35
+#define GPIO_SEL_35  (1ULL << GPIO_NUM_35)
+#endif
